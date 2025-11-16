5d9567ee73ce7e9adde69066ed802054f8bc3d77
Zach Smith <30701475+aryzach@users.noreply.github.com>
2025-11-15 22:11:55 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // important for GitHub Pages: use subpath in production
   base: mode === "development" ? "/" : "/sf-sauna-sanity/",
 
   server: {
